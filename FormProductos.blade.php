4352168f18a43daa279d0fffa226ed2539fbb736
Carlos Andres Orozco Palacio <tecnogeekman@gmail.com>
2015-04-08 16:55:29 -0500

    Cambio en Archivo
    agregado el texto prueba

--- FormProductos.blade.php
+++ FormProductos.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+Prueba
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta charset="UTF-8">
 	<title>Document</title>
